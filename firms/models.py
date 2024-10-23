87d5d0480b3b7fd035b8e5c3a5ee596f90336fe7
ARYAN <aryanjainak@gmail.com>
2024-10-23 21:16:04 +0530

    xyz

--- firms/models.py
+++ firms/models.py
@@ -18,7 +18,7 @@ class Firm(models.Model):
     email= models.EmailField(unique=True, null=True, blank=True)
     website_url= models.URLField(unique=True, null=True, blank=True)
 
-    members= models.ManyToManyField(User)
+    members= models.ManyToManyField(User, blank=True)
     assistant_id = models.CharField(max_length=40,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -65,6 +65,20 @@ class FirmInvite(models.Model):
     def __str__(self):
         return str(self.firm)
 
+STATES_CHOICES = (
+    (1,"Andhra Pradesh"),(2,"Arunachal Pradesh "),(3,"Assam"),
+    (4,"Bihar"),(5,"Chhattisgarh"),(6,"Goa"),(7,"Gujarat"),
+    (8,"Haryana"),(9,"Himachal Pradesh"),
+    (10,"Jammu and Kashmir "),(11,"Jharkhand"),(12,"Karnataka"),
+    (13,"Kerala"),(14,"Madhya Pradesh"),(15,"Maharashtra"),
+    (16,"Manipur"),(17,"Meghalaya"),(18,"Mizoram"),(19,"Nagaland"),
+    (20,"Odisha"),(21,"Punjab"),(22,"Rajasthan"),(23,"Sikkim"),
+    (24,"Tamil Nadu"),(25,"Telangana"),(26,"Tripura"),
+    (27,"Uttar Pradesh"),(28,"Uttarakhand"),(29,"West Bengal"),
+    (30,"Andaman and Nicobar Islands"),(31,"Chandigarh"),
+    (32,"Dadra and Nagar Haveli"),(32,"Daman and Diu"),
+    (33,"Lakshadweep"),(34,"Delhi"),(35,"Puducherry"),
+    )
 
 
 class Entity(models.Model):
@@ -72,9 +86,18 @@ class Entity(models.Model):
     name= models.CharField(max_length=100)
 
     industry_type=  models.IntegerField(choices=( (1, "Trading"), (2,"Manufacturing"), (3,"Service"), (4, "Others") ))
-    entity_type= models.IntegerField(choices=( (1, "Huf"), (2,"Proprietary"), (3, "Partnership"), (4, "Corporation")  ))
+    entity_type= models.IntegerField(choices=( (1, "HUF"), (2,"Proprietary"), (3, "Partnership"), (4, "Corporation")  ))
     gstin= models.CharField(max_length=15 ,unique=True, blank=True, null=True)
     pan_number= models.CharField(max_length=10,blank=True, null=True)
+    
+    state= models.IntegerField(choices=STATES_CHOICES)
+    mailing_address= models.TextField()
+    email= models.EmailField()
+
+
+    fy_beginning= models.DateField()
+    books_beginning= models.DateField()
+
     created_at= models.DateTimeField(auto_now_add=True)
      
 
